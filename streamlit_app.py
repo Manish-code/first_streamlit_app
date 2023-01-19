70875735a478a2f4256de906ae8dd4426414a84f
Manish Narayan <manish.amhs@gmail.com>
2023-01-19 20:28:10 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents new Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega # & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text(' Hard-Boiled Free-Range Egg')
  
