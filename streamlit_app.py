9b36fb4b3b3eb4b1f8e343c303c68ce60738094d
Manish Narayan <manish.amhs@gmail.com>
2023-01-19 20:27:31 +0530

    Update streamlit_app.py
    breakfast menu added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents new Healthy Dinner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega # & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text(' Hard-Boiled Free-Range Egg')
+ 
+ 
+
