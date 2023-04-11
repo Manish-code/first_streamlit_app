0b536a3da4f8f04344937a972834cd3a9fff1783
Manish Narayan <manish.amhs@gmail.com>
2023-04-11 17:09:41 +0530

    Create infrer_csv_snowflake.py

---
+++ infrer_csv_snowflake.py
@@ -0,0 +1,59 @@
+import csv
+import datetime
+
+def infer_data_types(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.reader(file)
+        headers = next(reader)
+        data_types = []
+        for i in range(len(headers)):
+            data_type = None
+            for row in reader:
+                if row[i] == '':
+                    continue
+                if data_type is None:
+                    if row[i].lower() in ('true', 'false'):
+                        data_type = bool
+                    elif row[i].isdigit():
+                        data_type = int
+                    else:
+                        try:
+                            float(row[i])
+                            data_type = float
+                        except ValueError:
+                            try:
+                                datetime.datetime.strptime(row[i], '%Y-%m-%d')
+                                data_type = datetime.date
+                            except ValueError:
+                                try:
+                                    datetime.datetime.strptime(row[i], '%Y-%m-%d %H:%M:%S.%f')
+                                    data_type = datetime.datetime
+                                except ValueError:
+                                    data_type = str
+                elif data_type == bool:
+                    if row[i].lower() not in ('true', 'false'):
+                        data_type = str
+                elif data_type == int:
+                    if not row[i].isdigit():
+                        try:
+                            float(row[i])
+                            data_type = float
+                        except ValueError:
+                            data_type = str
+                elif data_type == float:
+                    try:
+                        float(row[i])
+                    except ValueError:
+                        data_type = str
+                elif data_type == datetime.date:
+                    try:
+                        datetime.datetime.strptime(row[i], '%Y-%m-%d')
+                    except ValueError:
+                        data_type = str
+                elif data_type == datetime.datetime:
+                    try:
+                        datetime.datetime.strptime(row[i], '%Y-%m-%d %H:%M:%S.%f')
+                    except ValueError:
+                        data_type = str
+            data_types.append(data_type)
+    return data_types
